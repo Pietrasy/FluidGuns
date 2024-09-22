959464522df468d2968bb0064da41e093f16d3ea
ROZDUPCZATOR420\Gamedev <pietras1606@gmail.com>
2024-09-22 20:08:06 +0200

    Created native and blueprint class of fluid source. Import barrel static mesh. Created interaction widget that serves as widget component of fluid source actors. Set up InteractionWidget. Created blueprint interface for handling between fluid source and player.

---
+++ Source/FluidGuns/Private/Actors/FG_FluidSource.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright Pietras
+
+
+#include "Actors/FG_FluidSource.h"
+
+#include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
+
+AFG_FluidSource::AFG_FluidSource()
+{
+	// Create sphere component, set it as root component.
+	SphereCollision = CreateDefaultSubobject<USphereComponent>("SphereCollision");
+	SetRootComponent(SphereCollision);
+	// Create fluid source static mesh component and attach it to sphere collision component.
+	FluidSourceStaticMesh = CreateDefaultSubobject<UStaticMeshComponent>("FluidSourceStaticMesh");
+	FluidSourceStaticMesh->SetupAttachment(SphereCollision);
+	// Create widget component and attach it to fluid source static mesh component.
+	InteractionWidgetComponent = CreateDefaultSubobject<UWidgetComponent>("InteractionWidget");
+	InteractionWidgetComponent->SetupAttachment(FluidSourceStaticMesh);
+}
---
+++ Source/FluidGuns/Public/Actors/FG_FluidSource.h
@@ -0,0 +1,32 @@
+﻿// Copyright Pietras
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FG_FluidSource.generated.h"
+
+class UWidgetComponent;
+class USphereComponent;
+
+UCLASS()
+class FLUIDGUNS_API AFG_FluidSource : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AFG_FluidSource();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="FluidSource")
+	TObjectPtr<USphereComponent> SphereCollision = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="FluidSource")
+	TObjectPtr<UStaticMeshComponent> FluidSourceStaticMesh = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="FluidSource|Widget")
+	TObjectPtr<UWidgetComponent> InteractionWidgetComponent = nullptr;
+
+	// Class of interaction widget, that will be created as widget component of fluid source.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="FluidSource|Widget")
+	TSubclassOf<UUserWidget> InteractionWidgetClass = nullptr;
+};
