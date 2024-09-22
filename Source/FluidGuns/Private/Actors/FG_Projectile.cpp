cbe6eb69c173073aee900b83b7a50f15b0377304
ROZDUPCZATOR420\Gamedev <pietras1606@gmail.com>
2024-09-22 20:01:24 +0200

    Created native and blueprint class of projectile. Created data table of tank tag and fluid colors.

---
+++ Source/FluidGuns/Private/Actors/FG_Projectile.cpp
@@ -0,0 +1,17 @@
+﻿// Copyright Pietras
+
+
+#include "Actors/FG_Projectile.h"
+
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+AFG_Projectile::AFG_Projectile()
+{
+	// // Actor will not call Tick() every frame.
+	PrimaryActorTick.bCanEverTick = false;
+	// Create sphere component, set it as root component and set its collision profile.
+	SphereCollision = CreateDefaultSubobject<USphereComponent>("SphereCollision");
+	SetRootComponent(SphereCollision);
+	ProjectileComponent = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileComponent");
+}
---
+++ Source/FluidGuns/Public/Actors/FG_Projectile.h
@@ -0,0 +1,25 @@
+﻿// Copyright Pietras
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FG_Projectile.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+
+UCLASS()
+class FLUIDGUNS_API AFG_Projectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AFG_Projectile();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Projectile")
+	UProjectileMovementComponent* ProjectileComponent = nullptr;
+
+	UPROPERTY(EditAnywhere, Category="Projectile")
+	USphereComponent* SphereCollision = nullptr;
+};
